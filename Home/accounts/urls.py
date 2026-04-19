6556343116a46eda1c988af80a0c7cea2c26103c
shreya-212 <shreyaa.srs@gmail.com>
2026-04-19 09:13:47 +0530

    removed file

--- Home/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import RegisterView,Login_view
-
-urlpatterns = [
-    path('register/',RegisterView.as_view(),name='register'),
-    path('login/',Login_view.as_view(),name='login')
-]
